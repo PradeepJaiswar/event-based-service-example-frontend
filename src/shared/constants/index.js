08bab77d057dcf595d42aee0b48fa6046d4123a3
PradeepJaiswar <mado023@gmail.com>
2019-01-07 13:11:41 +0530

    add dev server api address and websocket address

--- src/shared/constants/index.js
+++ src/shared/constants/index.js
@@ -1,7 +1,7 @@
 export default Object.freeze({
   BASE_API_ENDPOINT_LOCAL: 'http://localhost:7000/api',
-  BASE_API_ENDPOINT_DEV: 'http://localhost:7000/api',
+  BASE_API_ENDPOINT_DEV: 'http://13.126.151.86:7000/api',
   WS_ENDPOINT_LOCAL: 'ws://localhost:8080/',
-  WS_ENDPOINT_DEV: 'ws://localhost:8080/',
+  WS_ENDPOINT_DEV: 'http://13.126.151.86:8080/',
   S3_BASE_ENDPOINT: 'https://s3.ap-south-1.amazonaws.com/open-table/',
 });
